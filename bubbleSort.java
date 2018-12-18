8b600032350755d9269d3bcc37b9066d1c19eb25
ArmandXiao <45375764+ArmandXiao@users.noreply.github.com>
2018-12-18 09:26:28 +0800

    Update bubbleSort.java

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,1 +1,11 @@
-
+public void maoPao(int[] arr){
+        int a = arr.length -1；
+        for(int i =0; i< a;){
+            for(int j = 0; j<a; j++){
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+            a--;
+        }
+    }
